85e306b13761967498da53e4d55702b5dc9eff64
smy5152 <shabbiryamani@gmail.com>
2021-03-23 21:30:46 +0200

    ADD: Email & Password columns in USER Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
-  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 4}, length: {maximum: 12}
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { in: 4..12 }
+  validates :email, presence: false, uniqueness: { case_sensitive: false }, length: {maximum: 50}
+  validates :password, presence: false, length: { in: 6..16 }
 end

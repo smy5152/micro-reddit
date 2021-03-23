1d49aadc80fc1a4040665a80515a01004a86e01e
Varun Sharma <67771380+Varun2323@users.noreply.github.com>
2021-03-24 01:21:37 +0530

    new user added with validations and database

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_22_212606) do
+ActiveRecord::Schema.define(version: 2021_03_23_161003) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
+    t.string "password"
   end
 
 end

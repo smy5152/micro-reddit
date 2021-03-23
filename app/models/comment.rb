af86660cda5d37f42c3962952e1e39887a31ac9c
smy5152 <shabbiryamani@gmail.com>
2021-03-24 00:52:37 +0200

    README Finalized, Rubocop fixes(futile)

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  validates :body, presence: true, length: { in: 10..100 }  
+  validates :body, presence: true, length: { in: 10..100 }
 end

e8f8079b4f9ccbed44d52b386211b3d8c09201c7
smy5152 <shabbiryamani@gmail.com>
2021-03-24 01:46:53 +0200

    Update: Associations Test to read multiple

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
-  has_many :comment
+  has_many :comments
 
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { in: 4..12 }
   validates :email, presence: false, length: { maximum: 50 }

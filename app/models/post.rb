9d5c268c4d91e43e289fed24d022f53c21193477
Varun Sharma <67771380+Varun2323@users.noreply.github.com>
2021-03-24 02:29:45 +0530

    user id
    changes added and relationship with associated validations added

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
+  has_one :user_id
+  validates_associated :user_id
+
   validates :title, presence: true, length: { maximum: 50 }
   validates :body, presence: true, length: { maximum: 50 }
 end
